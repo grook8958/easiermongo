e3dd60be71619eb7d399bfca5c0ef174b48bbb6a
grook8958 <clement.puks@gmail.com>
2022-05-25 19:14:41 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,9 +1,10 @@
 require('./test')()
 
 const SchemaBuilder = require('../src/builders/SchemaBuilder');
+const { SchemaFieldTypes } = require('../src/util/Constants');
 
 const builder = new SchemaBuilder();
 
-builder.addField(field => field.setName('_id').setRequired(true))
-
-builder.toJSON()
+builder.addField(field => field.setName('_id').setRequired(true).setType("string"))
+console.log(builder.toJSON())
+console.log(builder.toSchema())
