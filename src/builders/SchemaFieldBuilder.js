b81982023b129480205f25a98d18a763aefebfa7
grook8958 <78109323+grook8958@users.noreply.github.com>
2022-08-31 15:15:57 +0200

    Added TTL support
    - Added validation for the fields
    - Added ability to disable the validation

--- src/builders/SchemaFieldBuilder.js
+++ src/builders/SchemaFieldBuilder.js
@@ -1,6 +1,7 @@
 'use-strict';
 
 const { SchemaFieldTypes } = require('../util/Constants');
+const { TypeError } = require('../errors');
 /**
  * A builder to create a new field for a Schema
  */
@@ -14,12 +15,13 @@ class SchemaFieldBuilder {
 	 * @property {SchemaFieldType} type The type of this field
 	 * @property {boolean} required Wether this field is required
 	 * @property {any} default The default value of this field
+	 * @property {number} ttl The Time-To-Live (TTL) the document has
 	 */
 	/**
 	 * A builder to create a new field for a Schema
 	 * @param {SchemaFieldBuilderData} data
 	 */
-	constructor(data = {}) {
+	constructor(data = {}, skipValidation = false) {
 		/**
 		 * The name of this field
 		 * @type {string}
@@ -43,6 +45,18 @@ class SchemaFieldBuilder {
 		 * @type {any}
 		 */
 		this.default = data.default ?? null;
+
+		/**
+		 * The Time-To-Live (TTL) the document has in seconds.
+		 * @type {number}
+		 */
+		this.ttl = data.ttl ?? null;
+
+		/**
+		 * Wether to skip the validation of the fields
+		 * @type {Boolean}
+		 */
+		this.skipValidation = skipValidation;
 	}
 
 	/**
@@ -86,16 +100,30 @@ class SchemaFieldBuilder {
 	}
 
 	/**
+	 * Set's the Time-To-Live (TTL) the document has.
+	 * * Only works if the field type is `Date` and if the field has a value.
+	 * * TTL is reset every time the document is modified.
+	 * @param {number} ttl The Time-To-Live (TTL) in seconds.
+	 * @returns
+	 */
+	setTTL(ttl) {
+		this.ttl = ttl;
+		return this;
+	}
+
+	/**
 	 * Returns a JSON object representation of this class
 	 * @returns {Object}
 	 */
 	toJSON() {
-		const { name, required, type } = this;
+		const { name, required, type, ttl } = this;
+		this._validateOptions();
 		return {
 			name,
 			type,
 			required,
 			default: this.default,
+			expires: ttl
 		};
 	}
 
@@ -107,6 +135,21 @@ class SchemaFieldBuilder {
 	resolveFieldType(type) {
 		if (SchemaFieldTypes[type]) return SchemaFieldTypes[type];
 		else return type;
+	}
+	/**
+	 * Validate options
+	 * @param {boolean} skipValidation Wether to skip validation.
+	 * @private
+	 */
+	_validateOptions(skipValidation = this.skipValidation) {
+		if (skipValidation) return;
+		const { name, required, type, ttl } = this;
+		if (typeof name !== 'string') throw new TypeError('INVALID_TYPE', 'name', 'string');
+		if (required && typeof required !== 'boolean') throw new TypeError('INVALID_TYPE', 'required', 'boolean');
+		if (this.default && typeof this.default !== 'boolean') throw new TypeError('REQUIRED_FIELD', 'default', 'SchemaFieldBuilderData');
+		if (!type) throw new TypeError('REQUIRED_FIELD', 'type', 'SchemaFieldBuilderData');
+		if (ttl && typeof ttl !== 'number') throw new TypeError('INVALID_TYPE', 'ttl', 'number');
+		if (ttl && type != SchemaFieldTypes.DATE) throw new TypeError('INVALID_FIELD_TYPE', 'Date');
 	}
 }
 
