092e24bc4a3f69851ef8889f0af9f731b63ee7c7
grook8958 <78109323+grook8958@users.noreply.github.com>
2022-08-31 15:16:31 +0200

    Added two new error messages

--- src/errors/Messages.js
+++ src/errors/Messages.js
@@ -5,6 +5,8 @@ const { register } = require('./EasierMongoError');
 const Messages = {
 	MONGO_CLIENT_INVALID_OPTION: (prop, must, an = false) => `The ${prop} option must be ${an ? 'an' : 'a'} ${must}`,
 	INVALID_TYPE: (prop, must, an = false) => `${prop} must be ${an ? 'an' : 'a'} ${must}`,
+	INVALID_FIELD_TYPE: (must, an = false) => `The field type must be ${an ? 'an' : 'a'} ${must}`,
+	REQUIRED_FIELD: (field, on) => `"${field}" is required on ${on}.`
 };
 
 for (const [name, message] of Object.entries(Messages)) register(name, message);
