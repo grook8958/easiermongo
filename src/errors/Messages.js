28c904990b2f08d3a025d72283370868e4b0dc94
grook8958 <clement.puks@gmail.com>
2022-09-03 14:02:33 +0200

    New error messages.

--- src/errors/Messages.js
+++ src/errors/Messages.js
@@ -6,7 +6,8 @@ const Messages = {
 	MONGO_CLIENT_INVALID_OPTION: (prop, must, an = false) => `The ${prop} option must be ${an ? 'an' : 'a'} ${must}`,
 	INVALID_TYPE: (prop, must, an = false) => `${prop} must be ${an ? 'an' : 'a'} ${must}`,
 	INVALID_FIELD_TYPE: (must, an = false) => `The field type must be ${an ? 'an' : 'a'} ${must}`,
-	REQUIRED_FIELD: (field, on) => `"${field}" is required on ${on}.`
+	REQUIRED_FIELD: (field, on) => `"${field}" is required on ${on}.`,
+	DUPLICATE_ID: "The ID provided is already in use."
 };
 
 for (const [name, message] of Object.entries(Messages)) register(name, message);
